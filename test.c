bcfaf7ba68d9f6de26dcd7fab45e1edf70e81202
이동건 <94092857+fla0450@users.noreply.github.com>
2022-12-11 20:00:14 +0900

    알고리즘 과제 베타
    우성아 한번 3번 까지 만들었는데 한번 돌려보면 이상한 점이 있을거야 잘 고쳐봐
    \

---
+++ test.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+typedef struct String
+{
+    char str[100];
+}String;
+
+typedef struct Folder 
+{
+    String name;
+    struct String data[100];
+    struct Folder* next;
+}Folder;
+void printFolder(Folder* f){
+    if(f==NULL)
+    {
+        system("cls");
+        printf("폴더를 생성해 주세요");
+        Sleep(5000);
+        return;
+    }
+    Folder *p=f->next;
+    system("cls");
+    do{
+        if(p==NULL) break;
+        printf("%s\n",f->name.str);
+        p=p->next;
+    }while(p!=f->next);
+    Sleep(5000); 
+}
+
+Folder* makeFolder(Folder* f,String name){
+    Folder* newFolder =(Folder*)malloc(sizeof(Folder));
+    newFolder->name=name;
+    if(f==NULL){
+        f=newFolder;
+        f->next=f;
+    }
+    else{
+        newFolder->next=f->next->next;
+        f->next=newFolder;
+        f=newFolder;
+    }
+    return f;
+}
+int main(){
+    system("color 2");
+    Folder* Folder=NULL;
+    int appController=1;
+    int controller=0;
+    String folder_name;
+    while (appController)
+    {
+        system("cls");
+        printf("파일 정리 프로그램\n");
+        printf("1.설명서\n");
+        printf("2.폴더생성\n");
+        printf("3.폴더보기\n");
+        printf("4.폴더삭제\n");
+        printf("5.파일 삽입\n");
+        printf("6.파일 찾기\n");
+        printf("7.프로그램 종료\n");
+        scanf("%d",&controller);
+        switch (controller)
+        {
+        case 1:
+            system("cls");
+            printf("폴더를 먼저 생성하지 않으면 돌아가지 않습니다\n");
+            scanf("%d",&controller);
+            break;
+        case 2:
+            system("cls");
+            printf("폴더의 이름을 입력하세요\n");
+            scanf("%s",folder_name.str);
+            Folder=makeFolder(Folder,folder_name);
+        break;
+        case 3:
+            printFolder(Folder);
+        break;
+        default:
+            system("cls");
+            printf("잘못된 선택입니다\n");
+            appController=0;
+            break;
+        }
+    }
+    
+}
