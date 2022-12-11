8fbba2f3c3237efece446abc637cd07687a47c92
NameIsUser <111692254+NameIsUser06@users.noreply.github.com>
2022-12-11 20:33:24 +0900

    "에러 : printFolder 해결"

--- test.c
+++ test.c
@@ -2,54 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <Windows.h>
+
 typedef struct String
 {
     char str[100];
 }String;
 
-typedef struct Folder 
+typedef struct Folder
 {
     String name;
     struct String data[100];
     struct Folder* next;
 }Folder;
-void printFolder(Folder* f){
-    if(f==NULL)
+
+void printFolder(Folder* f) {
+    if (f == NULL)
     {
         system("cls");
         printf("폴더를 생성해 주세요");
         Sleep(5000);
         return;
     }
-    Folder *p=f->next;
+    Folder* p = f->next;
     system("cls");
-    do{
-        if(p==NULL) break;
-        printf("%s\n",f->name.str);
-        p=p->next;
-    }while(p!=f->next);
-    Sleep(5000); 
+    do {
+        if (p == NULL) break;
+        printf("%s\n", p->name.str);
+        p = p->next;
+    } while (p != f->next);
 }
 
-Folder* makeFolder(Folder* f,String name){
-    Folder* newFolder =(Folder*)malloc(sizeof(Folder));
-    newFolder->name=name;
-    if(f==NULL){
-        f=newFolder;
-        f->next=f;
+Folder* makeFolder(Folder* f, String name) {
+    Folder* newFolder = (Folder*)malloc(sizeof(Folder));
+    newFolder->name = name;
+    if (f == NULL) {
+        f = newFolder;
+        f->next = f;
     }
-    else{
-        newFolder->next=f->next->next;
-        f->next=newFolder;
-        f=newFolder;
+    else {
+        newFolder->next = f->next;
+        f->next = newFolder;
+        f = newFolder;
     }
     return f;
 }
-int main(){
+int main() {
     system("color 2");
-    Folder* Folder=NULL;
-    int appController=1;
-    int controller=0;
+    Folder* Folder = NULL;
+    int appController = 1;
+    int controller = 0;
     String folder_name;
     while (appController)
     {
@@ -62,29 +63,34 @@ int main(){
         printf("5.파일 삽입\n");
         printf("6.파일 찾기\n");
         printf("7.프로그램 종료\n");
-        scanf("%d",&controller);
+        printf("입력 >> ");
+        scanf("%d", &controller);
         switch (controller)
         {
         case 1:
             system("cls");
             printf("폴더를 먼저 생성하지 않으면 돌아가지 않습니다\n");
-            scanf("%d",&controller);
+            scanf("%d", &controller);
             break;
         case 2:
             system("cls");
             printf("폴더의 이름을 입력하세요\n");
-            scanf("%s",folder_name.str);
-            Folder=makeFolder(Folder,folder_name);
-        break;
+            scanf("%s", folder_name.str);
+            Folder = makeFolder(Folder, folder_name);
+            break;
         case 3:
             printFolder(Folder);
-        break;
+            Sleep(2000);
+            break;
+        case 7:
+            appController = 0;
+            break;
         default:
             system("cls");
             printf("잘못된 선택입니다\n");
-            appController=0;
+            appController = 0;
             break;
         }
     }
-    
+
 }
