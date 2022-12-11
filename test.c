b354f6398ed8ac9ab506a1248eedcb5da36013bc
NameIsUser <111692254+NameIsUser06@users.noreply.github.com>
2022-12-11 22:16:39 +0900

    "에러 : 파일 이름 출력 에러발생, 개선 : 파일 생성 완성 및 메뉴"

--- test.c
+++ test.c
@@ -16,18 +16,19 @@ typedef struct Folder
 }Folder;
 
 void printFolder(Folder* f) {
+    int i = 0;
     if (f == NULL)
     {
         system("cls");
         printf("폴더를 생성해 주세요");
-        Sleep(3000);
         return;
     }
     Folder* p = f->next;
     system("cls");
+    printf("--------------------존재 하는 폴더---------------\n");
     do {
         if (p == NULL) break;
-        printf("%s\n", p->name.str);
+        printf("%d. %s\n", ++i, p->name.str);
         p = p->next;
     } while (p != f->next);
 }
@@ -47,11 +48,10 @@ Folder* makeFolder(Folder* f, String name) {
     return f;
 }
 
-void addFolder(Folder* f) {
-    String file_name;
-    system("cls");
+void addFile(Folder* f) {
+    String file_name = { "" };
     printf("파일 입력 >> ");
-    scanf("%s", file_name);
+    scanf("%s", file_name.str);
     for (int i = 0; i < 100; i++) {
         if (f->data[i].str == NULL) {
             f->data[i] = file_name;
@@ -75,9 +75,58 @@ void searchFolder(Folder* f, String name) {
         if (p == NULL) break;
         if (!strcmp(p->name.str, name.str)) {
             addFile(p);
+            break;
         }
         p = p->next;
     } while (p != NULL);
+}
+
+void printFile(Folder* f) {
+    for (int i = 0; i < 100; i++) {
+        if (f->data[i].str != NULL) {
+            printf("%s\n", f->data[i].str);
+        }
+    }
+}
+
+void searchFile(Folder* f, String name) {
+    if (f == NULL) {
+        system("cls");
+        printf("폴더가 존재하지 않습니다\n");
+        Sleep(2000);
+        return;
+    }
+    Folder* p = f->next;
+    do {
+        if (p == NULL) break;
+        if (!strcmp(p->name.str, name.str)) {
+            printFile(p);
+            break;
+        }
+        p = p->next;
+    } while (p != NULL);
+}
+
+int menu(int controller) {
+    system("cls");
+    printf("--------------------파일 정리 프로그램---------------\n");
+    printf("**폴더를 먼저 생성하지 않으면 돌아가지 않습니다**    Made by 이동건, 조우성\n");
+    printf("\t1.폴더 생성\n");
+    printf("\t2.폴더 보기\n");
+    printf("\t3.폴더 삭제\n");
+    printf("\t4.파일 생성\n");
+    printf("\t5.파일 찾기\n");
+    printf("\t6.파일 보기\n");
+    printf("\t7.프로그램 종료\n");
+    printf("입력 >> ");
+    scanf("%d", &controller);
+    return controller;
+}
+
+String folderName(String folder_name) {
+    printf("폴더 이름을 입력하세요 >> ");
+    scanf("%s", folder_name.str);
+    return folder_name;
 }
 
 int main() {
@@ -85,41 +134,37 @@ int main() {
     Folder* Folder = NULL;
     int appController = 1;
     int controller = 0;
-    String folder_name;
+    String folder_name = { "" };
     while (appController)
     {
-        system("cls");
-        printf("파일 정리 프로그램\n");
-        printf("1.설명서\n");
-        printf("2.폴더생성\n");
-        printf("3.폴더보기\n");
-        printf("4.폴더삭제\n");
-        printf("5.파일 삽입\n");
-        printf("6.파일 찾기\n");
-        printf("7.프로그램 종료\n");
-        printf("입력 >> ");
-        scanf("%d", &controller);
+        controller = menu(controller);
         switch (controller)
         {
         case 1:
             system("cls");
-            printf("폴더를 먼저 생성하지 않으면 돌아가지 않습니다\n");
-            scanf("%d", &controller);
+            printf("--------------------폴더 생성---------------\n");
+            folder_name = folderName(folder_name);
+            Folder = makeFolder(Folder, folder_name);
             break;
         case 2:
             system("cls");
-            printf("폴더의 이름을 입력하세요\n");
-            scanf("%s", folder_name.str);
-            Folder = makeFolder(Folder, folder_name);
-            break;
-        case 3:
+            printf("--------------------폴더 보기---------------\n");
             printFolder(Folder);
             Sleep(2000);
             break;
         case 4:
-            scanf("%s", folder_name);
+            system("cls");
+            printf("--------------------파일 생성---------------\n");
+            printFolder(Folder);
+            folder_name = folderName(folder_name);
             searchFolder(Folder, folder_name);
             break;
+        case 6:
+            system("cls");
+            printf("--------------------파일 보기---------------\n");
+            printFolder(Folder);
+            folder_name = folderName(folder_name);
+            searchFile(Folder, folder_name);
         case 7:
             appController = 0;
             break;
@@ -130,5 +175,6 @@ int main() {
             break;
         }
     }
-
+    
+    return 0;
 }
